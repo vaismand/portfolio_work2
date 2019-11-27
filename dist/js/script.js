ce7d5dee6a8690f381b9f9b7249f6bc3b0985ff7
David <omgemjey@gmail.com>
2019-11-28 00:13:02 +0100

    carousel

---
+++ dist/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.modals_carousel').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
