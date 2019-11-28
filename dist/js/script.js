9941045569dffa5cc9e8930d23f2af582cea1049
David <omgemjey@gmail.com>
2019-11-28 21:36:58 +0100

    media

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,6 +7,15 @@ $(document).ready(function(){
         fade: true,
         cssEase: 'linear',
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+        responsive: [
+            {
+              breakpoint: 769,
+              settings: {
+                arrows: false,
+                dots: false
+              }
+            }
+          ]
     });
 });
